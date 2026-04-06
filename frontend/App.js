84906e366c361b4a82e5cf4ff273ff7e013af892
Sunil234554321 <sunilsarjal2@gmail.com>
2026-04-07 03:39:00 +0530

    Create initial App component for Smart Scanner

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🔥 Smart Scanner App</h1>
+      <p>App successfully connected 🚀</p>
+    </div>
+  );
+}
+
+export default App;
